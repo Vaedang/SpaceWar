311018a4d07a961bb3c2501cbfd4099dfda8e6e9
Neko <44874068+Vaedang@users.noreply.github.com>
2019-03-09 17:54:07 +0700

    Initial commit

---
+++ Assets/Script/BoltMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltMove : MonoBehaviour
+{
+    public Rigidbody rig;
+    public float speed;
+    void Start()
+    {
+        rig.velocity = transform.forward * speed;
+    }
+
+   
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public float speed;
+    public float xMin, xMax, zMin, zMax;
+    public float tilt;
+
+    public GameObject shot;
+    public Transform shotSpawner;
+    float nextFireTime, fireRate;
+    
+
+    void Start()
+    {
+        speed = 10;
+        xMin = -6.11f;
+        xMax = 6.07f;
+        zMin = -4.12f;
+        zMax = 14.09f;
+        tilt = 5;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("j") && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Instantiate(shot, shotSpawner.position, Quaternion.identity);
+        }    
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigid.velocity = movement*speed;
+
+        rigid.position = new Vector3(
+            Mathf.Clamp(rigid.position.x,xMin,xMax),
+            0.0f,
+            Mathf.Clamp(rigid.position.z,zMin,zMax)
+            );
+
+        rigid.rotation = Quaternion.Euler(0.0f, 0.0f, rigid.position.x * -tilt);
+    }
+}
